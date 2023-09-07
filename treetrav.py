3647a0593c14882e7c6b46be6ef75eeb5f9be900
Armin Maghsoudlou <amagh8540@gmail.com>
2023-09-07 15:20:49 -0400

    cors test

--- treetrav.py
+++ treetrav.py
@@ -1,8 +1,8 @@
 from app import create_app, db
 from app.models import User, Post
-
+from flask_cors import CORS
 app = create_app()
-
+CORS(app)
 
 @app.shell_context_processor
 def make_shell_context():
