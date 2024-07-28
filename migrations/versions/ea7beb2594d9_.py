7956d898c0e03a79e681dadb0b4622f3d5bf641a
Evan Chan <spagabtti@gmail.com>
2024-07-28 19:48:21 -0400

    migration

---
+++ migrations/versions/ea7beb2594d9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ea7beb2594d9
+Revises: 261bc1b55774
+Create Date: 2024-07-28 19:36:15.461965
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ea7beb2594d9'
+down_revision = '261bc1b55774'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
