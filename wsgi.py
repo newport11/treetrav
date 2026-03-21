e20a703749d2641a65036b8001fa2233163e4184
Armin Maghsoudlou <amagh8540@gmail.com>
2026-03-21 17:39:46 -0400

    remove portal and shardops, simplify run.py and wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,8 @@
 """WSGI entry point for production deployment.
 
 Usage:
-    gunicorn -c gunicorn.conf.py wsgi:application
+    gunicorn -c gunicorn.conf.py wsgi:app
 """
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
 from app import create_app
-from shardops import create_app as create_shardops
 
-treetrav_app = create_app()
-shardops_app = create_shardops()
-
-application = DispatcherMiddleware(treetrav_app, {
-    "/shardops": shardops_app,
-})
+app = create_app()
